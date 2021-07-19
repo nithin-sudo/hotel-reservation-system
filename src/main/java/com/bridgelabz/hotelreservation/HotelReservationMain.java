15fed22eb0631d51b17c95e78406907f7750860e
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-19 08:30:13 +0530

    Added: initial setup for Hotel Reservation System program.

---
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservation;
+
+public class HotelReservationMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Hotel Reservation System.");
+    }
+}
