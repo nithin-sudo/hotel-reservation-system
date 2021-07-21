09ce29f95717eea455e19d96f16b6f1efacde093
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-21 14:27:06 +0530

    Added:method and test case to find cheapest hotel.

--- src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -1,11 +1,29 @@
 package com.bridgelabz.hotelreservation;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class HotelReservation
 {
     ArrayList<Hotel> hotelList = new ArrayList<>();
+    /**
+     * Created a array list name as hotelList. Created a method addHotel to add the
+     * hotels in the array list.
+     *
+     * @param hotel
+     */
     public void addHotelDetails(Hotel hotel)
     {
         hotelList.add(hotel);
+    }
+    public Hotel getCheapestHotel(LocalDate startDate, LocalDate lastDate) {
+        long daysBetween = ChronoUnit.DAYS.between(startDate, lastDate);
+        int cheapRate;
+        Hotel cheapest = Collections.min(hotelList, Comparator.comparing(hotel -> hotel.getRates()));
+        cheapRate = (int) ((daysBetween + 1) * cheapest.getRates());
+        System.out.println("Cheapest Hotel Name: " + cheapest.getHotelName() + "\nTotal Rate: " + cheapRate);
+        return cheapest;
     }
 }
--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -1,10 +1,15 @@
 package com.bridgelabz.hotelreservation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class HotelReservationTest
 {
     HotelReservation hotelReservation = new HotelReservation();
+    /**
+     * This test method is use to check , if the hotel is added the return true .
+     */
     @Test
     public void addingMultipleHotels()
     {
@@ -15,5 +20,16 @@ public class HotelReservationTest
         hotelReservation.addHotelDetails(secondHotel);
         hotelReservation.addHotelDetails(ThirdHotel);
         Assertions.assertEquals("Lakewood",hotelReservation.hotelList.get(0).getHotelName());
+    }
+    /**
+     * This method is used to check the cheapest hotel between the given data range
+     *
+     */
+    @Test
+    public void givenDateRange_ShouldReturnCheapestHotel() {
+        LocalDate startDate = LocalDate.of(2020, Month.SEPTEMBER, 10);
+        LocalDate lastDate = LocalDate.of(2020, Month.SEPTEMBER, 11);
+        Hotel cheapestHotel = hotelReservation.getCheapestHotel(startDate, lastDate);
+        Assertions.assertEquals("LakeWood", cheapestHotel.getHotelName());
     }
 }
